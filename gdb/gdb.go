1f9675c17e3ac4f5288898cb0aac4aaef51a8bd1
ben <git@ben.iagu.net>
2015-11-09 12:42:02 +1030

    try again to fix gdb arg slice race

--- gdb/gdb.go
+++ gdb/gdb.go
@@ -381,7 +381,8 @@ func init() {
 func (e *Engine) Run(command []string, filename string, memlimit, timeout int) (crash.Info, error) {
 
 	var cmd *exec.Cmd
-	args := gdbArgs[:]
+	args := []string{}
+	copy(args, gdbArgs)
 
 	sub := 0
 	for i, elem := range command {
